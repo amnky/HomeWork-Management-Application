472b206e27dfb9abf2a8577e3636467c458d9681
Aman Yadav <95027373+amnky@users.noreply.github.com>
2023-09-22 13:36:05 +0530

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://homework-api.onrender.com/',
+    baseUrl: 'https://homework-api.onrender.com',
     
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
